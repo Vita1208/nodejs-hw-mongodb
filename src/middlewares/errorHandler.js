f007bb0a3e1ff608349f58042c4d08c8584f777b
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-09-23 13:58:09 +0300

    errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,37 @@
-const errorHandler = (error, req, res, next) => {
-  const { status = 500, message } = error;
-  res.status(status).json({
-    message,
+import { HttpError } from 'http-errors';
+
+// Middleware for error handling for 500 status
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status:500,
+    message: '500: Server error, please try again later.',
+    data: err.message || 'Unknown error occurred.',
   });
 };
 
 export default errorHandler;
+
+//const errorHandler = (error, req, res, next) => {
+ // const { status = 500, message } = error;
+ // res.status(status).json({
+ //   status,
+  //  message,
+ //   data: null,
+ // });
+//};
+
+//export default errorHandler;
 
 // const errorHandler = (error, req, res, next) => {
 //   res.status(500).json({
