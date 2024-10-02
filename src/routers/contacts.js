b41b9ba89e0cf4170ede37739c6a6997495ed6cf
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-10-02 19:02:24 +0300

    Fixed contact search by id and userId, replaced findById with findOne

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import * as contactControllers from '../controllers/contacts.js';
+import authenticate from '../middlewares/authenticate.js';
+import isValidId from '../middlewares/isValidId.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../utils/validateBody.js';
-import { contactAddSchema, contactPatchSchema } from '../validation/contacts.js';
-import isValidId from "../middlewares/isValidId.js";
-
-
-
+import {contactAddSchema,contactPatchSchema} from '../validation/contacts.js';
 const contactsRouter = Router();
+contactsRouter.use(authenticate);
 
 contactsRouter.get('/', ctrlWrapper(contactControllers.getAllContactsController));
 
