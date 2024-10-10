8366003657481338ef11a724de93d502cd384606
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-10-10 22:18:27 +0300

    Initial commit for hw7-swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import authRouter from './routers/auth.js';
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import logger from "./middlewares/logger.js";
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 
 export const setupServer = () => {
@@ -16,8 +18,11 @@ export const setupServer = () => {
     app.use(cors());
     app.use(express.json());
     app.use(cookieParser());
+    app.use(express.static('uploads'));
     app.use("/auth", authRouter);
     app.use("/contacts", contactsRouter);
+    app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
     app.use(notFoundHandler);
     app.use(errorHandler);
   
